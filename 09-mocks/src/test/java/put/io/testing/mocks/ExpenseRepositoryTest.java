cfd2d9c61a1104a3503daf44f7888be531338dbe
Teablack <34287403+Teablack@users.noreply.github.com>
2020-12-28 14:30:38 +0100

    Add files via upload

---
+++ 09-mocks/src/test/java/put/io/testing/mocks/ExpenseRepositoryTest.java
@@ -0,0 +1,58 @@
+package put.io.testing.mocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+//import org.junit.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import put.io.students.fancylibrary.database.FancyDatabase;
+import put.io.students.fancylibrary.database.IFancyDatabase;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ExpenseRepositoryTest {
+    private ExpenseRepository expenseRepository;
+    private List<Expense> expenses;
+
+    @Test
+    void loadExpensesTest(){
+        //Tworzenie
+        IFancyDatabase mockedDB = mock(IFancyDatabase.class);
+        InOrder inOrder = inOrder(mockedDB);
+        //Konfiguracja
+        when(mockedDB.queryAll()).thenReturn(Collections.emptyList());
+        //Interakcja
+        expenseRepository = new ExpenseRepository(mockedDB);
+        expenseRepository.loadExpenses();
+        //Weryfikacja
+        inOrder.verify(mockedDB).connect();
+        inOrder.verify(mockedDB).queryAll();
+        inOrder.verify(mockedDB).close();
+        assertEquals(0, expenseRepository.getExpenses().size());
+        }
+
+    @Test
+    void saveExpensesTest() {
+        //Tworzenie
+        IFancyDatabase mockedDB = mock(IFancyDatabase.class);
+        InOrder inOrder = inOrder(mockedDB);
+        //Konfiguracja
+        when(mockedDB.queryAll()).thenReturn(Collections.emptyList());
+        //Interakcja
+        expenseRepository = new ExpenseRepository(mockedDB);
+        IntStream.range(0, 5).forEach(i -> expenseRepository.addExpense(new Expense()));
+        expenseRepository.saveExpenses();
+        //Weryfikacja
+        verify(mockedDB, times(5)).persist(any(Expense.class));
+        inOrder.verify(mockedDB).connect();
+        inOrder.verify(mockedDB).close();
+    }
+
+}
